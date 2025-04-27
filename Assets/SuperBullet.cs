fad0d0f3d65e8545f54b101a36371d3e2bb232e2
Brookuu <m0918664055@gmail.com>
2025-04-27 11:20:19 +0800

    1

--- Assets/SuperBullet.cs
+++ Assets/SuperBullet.cs
@@ -7,33 +7,29 @@ public class SuperBullet : MonoBehaviour
 {
     public float speed = 250f;
 
-    private float initialX;
+    private float initialY;
     private float screenWidthWorld;
 
     public float maxScale = 6f;
     public float minScale = 1f;
-    private float scaleDistanceThreshold;
 
-    public float hitDistance = 50f; // 距離小於這個，就算打中
+    private float scaleDistanceThreshold = Screen.height; // 用來調整縮放比例範圍
+    public float hitDistance = 30f; // 距離小於這個，就算打中
 
     void Start()
     {
-        initialX = transform.position.x;
+        initialY = transform.position.y; // 記錄初始位置
 
-        // 螢幕右邊界轉成世界座標
-        Vector3 screenRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
-        screenWidthWorld = screenRight.x + 1f;
-
-        scaleDistanceThreshold = screenWidthWorld * 0.5f;
     }
 
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
 
         AdjustScaleBasedOnPosition();
 
-        if (transform.position.x > screenWidthWorld)
+        // 超出畫面就銷毀
+        if (transform.position.y > Screen.height)
         {
             Destroy(gameObject);
         }
@@ -41,11 +37,18 @@ public class SuperBullet : MonoBehaviour
         CheckHitSentences();
     }
 
+
+    // 根據 Y 位置調整子彈的縮放
     private void AdjustScaleBasedOnPosition()
     {
-        float distanceFromInitial = Mathf.Abs(transform.position.x - initialX);
+        // 計算子彈的 Y 位置和初始 Y 位置的距離
+        float distanceFromInitial = Mathf.Abs(transform.position.y - initialY);
+
+        // 根據子彈的 Y 距離來計算縮放比例
         float scale = Mathf.Lerp(maxScale, minScale, distanceFromInitial / scaleDistanceThreshold);
-        transform.localScale = new Vector3(scale, scale, 1f);
+
+        // 設定子彈的縮放
+        transform.localScale = new Vector3(scale, scale, 1);
     }
 
     private void CheckHitSentences()
@@ -58,7 +61,6 @@ public class SuperBullet : MonoBehaviour
                 // 打中
                 FindObjectOfType<SentenceManager>().RemoveSentence(sentence);
                 sentence.DestroySentence();
-                Destroy(gameObject);
                 break;
             }
         }
